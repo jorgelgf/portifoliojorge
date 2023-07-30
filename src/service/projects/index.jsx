e7ebd798fa19a7e11041241e9e5c7d57937325aa
jorgelgf <jlgurgelfernandes@gmail.com>
2023-07-30 14:35:09 -0300

    Novas informações: adc projetos no objeto projects

--- src/service/projects/index.jsx
+++ src/service/projects/index.jsx
@@ -1,44 +1,49 @@
 export const projects = [
   {
-    name: "SujeitoPizza",
-    info: "2022 - projeto administrativo para pizzaria",
-    github: "https://github.com/jorgelgf/pizzaria",
-    page: "",
+    name: "Gbook",
+    info: "2021 - Site para encontrar seu livro",
+    github: "https://github.com/jorgelgf/gbook",
+  },
+  {
+    name: "Site Pokemon",
+    info: "2021 - Projeto pokedex",
+    github: "https://github.com/jorgelgf/pokemon-site",
+  },
+
+  {
+    name: "Page Sunny Lenses",
+    info: "2022 - Landing page",
+    github: "https://github.com/jorgelgf/sunny-lenses",
+  },
+  {
+    name: "Minhas notas",
+    info: "2022 - Bloco de notas online",
+    github: "https://github.com/jorgelgf/notes",
+  },
+  {
+    name: "Calculator",
+    info: "2022 - Calculadora",
+    github: "https://github.com/jorgelgf/my-Calculator",
   },
   {
     name: "SujeitoPizza",
-    info: "2022 - projeto administrativo para pizzaria",
+    info: "2022 - Projeto comercial para pizzaria",
     github: "https://github.com/jorgelgf/pizzaria",
-    page: "",
   },
   {
-    name: "SujeitoPizza",
-    info: "2022 - projeto administrativo para pizzaria",
-    github: "https://github.com/jorgelgf/pizzaria",
-    page: "",
+    name: "Sua Água",
+    info: "2022 - Projeto comercial para pedidos de água mineral",
+    github: "https://github.com/jorgelgf/agua",
+  },
+
+  {
+    name: "WaMe",
+    info: "2023 - Página envio de mensagens para whatsAppWeb ",
+    github: "https://github.com/jorgelgf/wame/blob/main/README.md",
   },
   {
-    name: "SujeitoPizza",
-    info: "2022 - projeto administrativo para pizzaria",
-    github: "https://github.com/jorgelgf/pizzaria",
-    page: "",
-  },
-  {
-    name: "SujeitoPizza",
-    info: "2022 - projeto administrativo para pizzaria",
-    github: "https://github.com/jorgelgf/pizzaria",
-    page: "",
-  },
-  {
-    name: "SujeitoPizza",
-    info: "2022 - projeto administrativo para pizzaria",
-    github: "https://github.com/jorgelgf/pizzaria",
-    page: "",
-  },
-  {
-    name: "SujeitoPizza",
-    info: "2022 - projeto administrativo para pizzaria",
-    github: "https://github.com/jorgelgf/pizzaria",
-    page: "",
+    name: "Cronologia",
+    info: "2023 - Página para criação de cronologia ",
+    github: "https://github.com/jorgelgf/cronologia",
   },
 ];
